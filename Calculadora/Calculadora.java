5d64f88d2d65be5dfaa2f1888896b9eaf5de7ae1
Aaron403-dot <eyeaaron1@gmail.com>
2025-03-25 10:44:25 +0000

    cambiado los metodos de estaticos a normal y casteado la clase calculadora a metodo main

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -10,49 +10,50 @@ public class Calculadora {
             double Var2;
             boolean looper = true;
             Scanner IDNumero = new Scanner(System.in);
+            Calculadora cal = new Calculadora();
+
             while (looper) {
                 System.out.println("introduzca el tipo de operacion de calculo que desea");
                 System.out.println("[1]SUMA [2]RESTA [3]MULTIPLICACION [4]DIVISION [5]RESTOS [0]SALIR");
                 int Opcion = IDNumero.nextInt();
                 switch (Opcion) {
-                    case 1:
+                    case 1 -> {
                         System.out.println("Introduzca el primer valor");
                         Var1 = IDNumero.nextDouble();
                         System.out.println("Introduzca el segundo valor");
                         Var2 = IDNumero.nextDouble();
-                        suma(Var1, Var2);
-                        break;
-                    case 2:
+                        cal.suma(Var1, Var2);
+                    }
+                    case 2 -> {
                         System.out.println("Introduzca el primer valor");
                         Var1 = IDNumero.nextDouble();
                         System.out.println("Introduzca el segundo valor");
                         Var2 = IDNumero.nextDouble();
-                        resta(Var1, Var2);
-                        break;
-                    case 3:
+                        cal.resta(Var1, Var2);
+                        
+                    }
+                    case 3 -> {
                         System.out.println("Introduzca el primer valor");
                         Var1 = IDNumero.nextDouble();
                         System.out.println("Introduzca el segundo valor");
                         Var2 = IDNumero.nextDouble();
-                        multi(Var1, Var2);
-                        break;
-                    case 4:
+                        cal.multi(Var1, Var2);
+                    }
+                    case 4 -> {
                         System.out.println("Introduzca el primer valor");
                         Var1 = IDNumero.nextDouble();
                         System.out.println("Introduzca el segundo valor");
                         Var2 = IDNumero.nextDouble();
-                        division(Var1, Var2);
-                        break;
-                    case 5:
+                        cal.division(Var1, Var2);
+                    }
+                    case 5 -> {
                         System.out.println("Introduzca el primer valor");
                         Var1 = IDNumero.nextDouble();
                         System.out.println("Introduzca el segundo valor");
                         Var2 = IDNumero.nextDouble();
-                        restos(Var1, Var2);
-                        break;
-                    default:
-                        looper = false;
-                        break;
+                        cal.restos(Var1, Var2);
+                    }
+                    default -> looper = false;
                 }
             }
             IDNumero.close();
@@ -61,14 +62,14 @@ public class Calculadora {
             }
     }
 
-    private static double suma(double Var1, double Var2) {
+    private double suma(double Var1, double Var2) {
         
         double suma = Var1 +Var2;
         System.out.println("El resulado de la suma es: " + suma);
         return suma;
     }
 
-    private static double resta(double Var1, double Var2) {
+    private double resta(double Var1, double Var2) {
         
         double resta = Var1 -Var2;
         System.out.println("El resulado de la resta es: " + resta);
@@ -76,13 +77,13 @@ public class Calculadora {
 
     }
 
-    private static double multi(double Var1, double Var2) {
+    private double multi(double Var1, double Var2) {
         double multi = Var1 * Var2;
         System.out.println("El resulado de la multiplicacion es: " + multi);
         return multi;
     }
 
-    private static double division(double Var1, double Var2) {
+    private double division(double Var1, double Var2) {
         try{
             if (Var1 == 0 | Var2 == 0) {
                 System.err.println("No puede dividir por cero");
@@ -97,7 +98,7 @@ public class Calculadora {
         }
     }
 
-    private static double restos(double Var1, double Var2) {
+    private double restos(double Var1, double Var2) {
         try{
             if (Var1 == 0 | Var2 == 0) {
                 System.err.println("No puede dividir por cero");
