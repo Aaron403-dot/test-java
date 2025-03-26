88601795b540407b55a15ad0e27fff1fa17442bb
Aaron403-dot <eyeaaron1@gmail.com>
2025-03-26 09:21:44 +0000

    optimizado asteriscos

--- TirarDados.java
+++ TirarDados.java
@@ -31,58 +31,55 @@ public class TirarDados {
     
     private void AnalizarResultados(List<Integer> Lista){
         int D2 = 0;
-        String D2Asterisk = "";
         int D3 = 0;
-        String D3Asterisk = "";
         int D4 = 0;
-        String D4Asterisk = "";
         int D5 = 0;
-        String D5Asterisk = "";
         int D6 = 0;
-        String D6Asterisk = "";
         int D7 = 0;
-        String D7Asterisk = "";
         int D8 = 0;
-        String D8Asterisk = "";
         int D9 = 0;
-        String D9Asterisk = "";
         int D10 = 0;
-        String D10Asterisk = "";
         int D11 = 0;
-        String D11Asterisk = "";
         int D12 = 0;
-        String D12Asterisk = "";
         for (int N : Lista) {
             switch (N) {
-                case 2 -> {D2++; D2Asterisk += "*";}
-                case 3 -> {D3++; D3Asterisk += "*";}
-                case 4 -> {D4++; D4Asterisk += "*";}
-                case 5 -> {D5++; D5Asterisk += "*";}
-                case 6 -> {D6++; D6Asterisk += "*";}
-                case 7 -> {D7++; D7Asterisk += "*";}
-                case 8 -> {D8++; D8Asterisk += "*";}
-                case 9 -> {D9++; D9Asterisk += "*";}
-                case 10 -> {D10++; D10Asterisk += "*";}
-                case 11 -> {D11++; D11Asterisk += "*";}
-                case 12 -> {D12++; D12Asterisk += "*";}
+                case 2 -> D2++;
+                case 3 -> D3++;
+                case 4 -> D4++;
+                case 5 -> D5++;
+                case 6 -> D6++;
+                case 7 -> D7++;
+                case 8 -> D8++;
+                case 9 -> D9++;
+                case 10 -> D10++;
+                case 11 -> D11++;
+                case 12 -> D12++;
                 default -> throw new AssertionError();
             }
         }
-        System.out.println("Ha salido el 2: " + D2 + " " + D2Asterisk);
-        System.out.println("Ha salido el 3: " + D3 + " " + D3Asterisk);
-        System.out.println("Ha salido el 4: " + D4 + " " + D4Asterisk);
-        System.out.println("Ha salido el 5: " + D5 + " " + D5Asterisk);
-        System.out.println("Ha salido el 6: " + D6 + " " + D6Asterisk);
-        System.out.println("Ha salido el 7: " + D7 + " " + D7Asterisk);
-        System.out.println("Ha salido el 8: " + D8 + " " + D8Asterisk);
-        System.out.println("Ha salido el 9: " + D9 + " " + D9Asterisk);
-        System.out.println("Ha salido el 10: " + D10 + " " + D10Asterisk);
-        System.out.println("Ha salido el 11 " + D11 + " " + D11Asterisk);
-        System.out.println("Ha salido el 12: " + D12 + " " + D12Asterisk);
+        System.out.println("Ha salido el 2: " + D2 +  GenerarAsteriscos(D2));
+        System.out.println("Ha salido el 3: " + D3 +  GenerarAsteriscos(D3));
+        System.out.println("Ha salido el 4: " + D4 +  GenerarAsteriscos(D4));
+        System.out.println("Ha salido el 5: " + D5 +  GenerarAsteriscos(D5));
+        System.out.println("Ha salido el 6: " + D6 +  GenerarAsteriscos(D6));
+        System.out.println("Ha salido el 7: " + D7 +  GenerarAsteriscos(D7));
+        System.out.println("Ha salido el 8: " + D8 +  GenerarAsteriscos(D8));
+        System.out.println("Ha salido el 9: " + D9 +  GenerarAsteriscos(D9));
+        System.out.println("Ha salido el 10: " + D10 +  GenerarAsteriscos(D10));
+        System.out.println("Ha salido el 11 " + D11 +  GenerarAsteriscos(D11));
+        System.out.println("Ha salido el 12: " + D12 +  GenerarAsteriscos(D12));
 
         Collections.sort(Lista);
         System.out.println(Lista);
-        
+    }
+
+    private String GenerarAsteriscos(int GA)
+    {
+        String Asteriscos = " | ";
+        for (int i = 0; i < GA; i++) {
+            Asteriscos += "*";
+        }
+        return Asteriscos;
     }
 
 }
